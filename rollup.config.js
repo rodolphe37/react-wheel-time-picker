cd350364cf1789e360498f6102c21f6dd526103f
rodolphe <rodolphe@MBP-de-rodolphe.lan>
2024-09-10 12:45:33 +0200

    Fix library

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,33 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
-import dts from "rollup-plugin-dts";
-import terser from '@rollup/plugin-terser';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import postcss from "rollup-plugin-postcss";
 
-export default [{
-    input: 'src/index.tsx',
-    output: [
-        {
-            file: 'dist/bundle.cjs.js',
-            format: 'cjs',
-            sourcemap: true,
-            exports: 'auto'
-        },
-        {
-            file: 'dist/bundle.esm.js',
-            format: 'esm',
-            sourcemap: true
-        }
-    ],
-    plugins: [
-        peerDepsExternal(),
-        resolve(),
-        commonjs(),
-        typescript({ tsconfig: './tsconfig.json' }),
-        terser(),
-        postcss()
-    ],
-    external: ['react', 'react-dom', 'react-player', /\.css$/]  // Ensure these are not bundled
-},
-]
+
+export default {
+  input: "src/index.tsx",
+  output: [
+    {
+      file: "dist/bundle.cjs.js",
+      format: "cjs",
+      sourcemap: true,
+      exports: "auto",
+    },
+    {
+      file: "dist/bundle.esm.js",
+      format: "esm",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({ tsconfig: "./tsconfig.json" }),
+    terser(),
+    postcss(),
+  ],
+  external: ["react", "react-dom", "react-player", /\.css$/], // Ensure these are not bundled
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import TimePicker from './components/TimePicker';
 import { TimePickerProps } from './components/TimePicker.type';
+import './styles/react-wheel-time-picker.css'
 
 
 export { TimePicker };
