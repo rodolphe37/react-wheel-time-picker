8afc3de1fea57a3034cd0f9f4becf5d9943372ff
rodolphe <rodolphe@MBP-de-rodolphe.lan>
2024-09-10 11:29:18 +0200

    Fix library

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import {cssModules} from 'rollup-plugin-css-modules';
 import css from "rollup-plugin-import-css";
